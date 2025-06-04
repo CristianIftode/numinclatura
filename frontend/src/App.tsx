a4fc2a3f1628599d31357ef62be9d297492ecb94
Sapphire <iftode2014@yandex.ru>
2025-06-04 10:18:54 +0300

    Второй этап

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { store } from './store';
 import Login from './pages/Login';
 import ProtectedRoute from './components/ProtectedRoute';
+import { Link } from 'react-router-dom';
 
 // Временный компонент для главной страницы
 const Home = () => <div className="p-4">Главная страница</div>;
@@ -11,6 +12,28 @@ const Home = () => <div className="p-4">Главная страница</div>;
 function App() {
   return (
     <Provider store={store}>
+
+    <div className='flex flex-row'>
+
+    <div className='max-w-[300px] w-full bg-blue-500 flex flex-col justify-between'>
+     <div className='flex flex-col'>
+       <div>
+        Нуминклатура
+       </div>
+
+       <div>
+        <ul>
+          <li><Link to="/">Главная</Link></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+       </div>
+     
+     </div>
+    </div>
+
+    <div className='w-full'>
       <Router>
         <Routes>
           <Route path="/login" element={<Login />} />
@@ -25,6 +48,9 @@ function App() {
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </Router>
+    </div>        
+    </div>
+
     </Provider>
   );
 }
